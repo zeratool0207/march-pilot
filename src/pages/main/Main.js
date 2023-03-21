8c6a87985bbdf5399a0026a498c229c11993cfef
zera <zzz@example.com>
2023-03-21 17:13:11 +0900

    Main.js counting 자동상승

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,23 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 
 const Main = () => {
 
-  const [account, setAccount] = useState({});
+  let start = 0;
+  const [ account, setAccount ] = useState({});
+  const [ value, setValue ] = useState(null);
+  const [ end, setEnd ] = useState(0);
+  
+  const ref = useRef(start);
+
+  const counter = end/200;
 
   useEffect( () => {
     axiosGet();
   },[])
+
+  useEffect( () => {
+    let isMounted = true;
+    if ( isMounted) {
+      count ();
+    }
+    return () => (isMounted = false);
+},[end]);
 
   const axiosGet = async () => {
     try {
       const response = await axios.get('https://account-31643-default-rtdb.firebaseio.com/accounts.json');
       console.log(response.data.a3);
       setAccount(response.data.a3);
+      setEnd(response.data.a3.total);
     } catch(e) {
       console.log(e);
     }
+  }
+
+  const count  = () => {
+      if (ref.current < end ) {
+        const result = Math.ceil(ref.current + counter);
+        const resultComma = result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        if ( result > end ) return setValue(end);
+        setValue(resultComma);
+        ref.current = result;
+    }
+    setTimeout(count, 10);
   }
 
   return (
@@ -26,7 +53,7 @@ const Main = () => {
         <div>{account.accNum}</div>
         <div>{account.name}</div>
         <div>{account.status}</div>
-        <div>{account.total}</div>
+        <div>{value} 원</div>
       </div>
     </>
   )
